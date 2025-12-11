06075bc1034db58ea7a26bb2429e8a96335934ab
Filippo <filippo.greppi2@studio.unibo.it>
2025-12-11 18:34:58 +0100

    Adds login.php

---
+++ www/login.php
@@ -0,0 +1,28 @@
+<?php
+require_once 'bootstrap.php';
+
+// Base Template
+$templateParams["titolo"] = "Mensa Campus - Login";
+
+if (!isUserLoggedIn()) {
+    $templateParams["nav_items"][] = getNewNavItem("Home", "index.php", "bi bi-house-door");
+    $templateParams["nav_items"][] = getNewNavItem("Menu", "menu.php", "bi bi-book");
+    $templateParams["nav_items"][] = getNewNavItem("Accedi", "login.html", "bi bi-box-arrow-in-right");
+    $templateParams["link_utili"][] = array(
+        "name" => "Menu",
+        "link" => "menu.php",
+    );
+    $templateParams["link_utili"][] = array(
+        "name" => "Accedi",
+        "link" => "login.php",
+    );
+    $templateParams["link_utili"][] = array(
+        "name" => "Registrati",
+        "link" => "register.php",
+    );
+}
+
+$templateParams["content"] = "template/content-login.php";
+
+require 'template/base-user.php';
+?>
