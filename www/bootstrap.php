3364d78f37f84889e73d69d4285b5e4415105470
Filippo Greppi <145936486+unibo-FilippoGreppi@users.noreply.github.com>
2025-12-19 16:08:59 +0100

    Prevent direct access and auto-update future TimeSlots

--- www/bootstrap.php
+++ www/bootstrap.php
@@ -1,8 +1,16 @@
 <?php
+// Prevent direct access
+if (php_sapi_name() !== 'cli' && realpath(__FILE__) === realpath($_SERVER['SCRIPT_FILENAME'])) {
+	http_response_code(403);
+	exit('Access denied');
+}
+
 session_start();
 define("UPLOAD_DIR", "./upload/");
 define("IN_APP", true);
 require_once("db/database.php");
 require_once("utils/functions.php");
 $dbh = new DatabaseHelper("localhost", "root", "", "cafeteria", 3306);
+
+$dbh->updateTimeSlots();
 ?>
