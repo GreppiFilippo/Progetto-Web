2416f635d96c4596b7d4a7ff7f0785dbe536cbf0
Filippo Greppi <145936486+unibo-FilippoGreppi@users.noreply.github.com>
2025-12-19 16:37:14 +0100

    now the dbms updates time_slots

--- www/bootstrap.php
+++ www/bootstrap.php
@@ -11,6 +11,4 @@ define("IN_APP", true);
 require_once("db/database.php");
 require_once("utils/functions.php");
 $dbh = new DatabaseHelper("localhost", "root", "", "cafeteria", 3306);
-
-$dbh->updateTimeSlots();
 ?>
