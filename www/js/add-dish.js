7f38a4fc3116474195788c855fd7e8e3a73eed1c
Marcello Spagnoli <marcello.spagnoli2@studio.unibo.it>
2026-01-05 15:42:28 +0100

    fixed text and icons in add/edit dish

--- www/js/add-dish.js
+++ www/js/add-dish.js
@@ -1,4 +1,6 @@
-document.addEventListener("DOMContentLoaded", () => {   
+document.addEventListener("DOMContentLoaded", () => { 
+    
+    document.getElementById("confirm").innerHTML = `<i class="bi bi-check-lg me-2"></i>Aggiungi Piatto`;
     // Bottone annulla: va alla dashboard
     const cancelBtn = document.getElementById("cancel");
     if (cancelBtn) {
