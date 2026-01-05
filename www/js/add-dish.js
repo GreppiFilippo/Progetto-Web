d5a7d4540fcbcb249c1e071f1d9e387c7b55e398
Marcello Spagnoli <marcello.spagnoli2@studio.unibo.it>
2026-01-05 16:17:36 +0100

    changed icon and fixed item stocks

--- www/js/add-dish.js
+++ www/js/add-dish.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => { 
     
     document.getElementById("confirm").innerHTML = `<i class="bi bi-check-lg me-2"></i>Aggiungi Piatto`;
-    document.getElementById("title").innerHTML = `<i class="bi bi-plus admin-icon fs-2" aria-hidden="true"></i> Aggiungi Piatto`;
+    document.getElementById("title").innerHTML = `<i class="bi bi-plus-circle admin-icon fs-2" aria-hidden="true"></i> Aggiungi Piatto`;
     // Bottone annulla: va alla dashboard
     const cancelBtn = document.getElementById("cancel");
     if (cancelBtn) {
